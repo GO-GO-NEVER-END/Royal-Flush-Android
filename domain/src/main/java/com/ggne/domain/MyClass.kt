d8f2019e4942120aa08e7455ec86699eeca7989c
soopeach <hsjeon584@gmail.com>
2023-03-27 17:12:48 +0900

    [main] init: commit message
    Co-authored-by: yangsooplus <yangsooplus@naver.com>
    Co-authored-by: bngsh <hee132@korea.ac.kr>
    Co-authored-by: ldh019 <ldh019@naver.com>
    Co-authored-by: jeeminimini <jiminn821@naver.com>
    Co-authored-by: swjw13 <swjw13@gmail.com>

---
+++ domain/src/main/java/com/ggne/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.ggne.domain
+
+class MyClass {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://naver.jfrog.io/artifactory/maven/")
+        maven("https://jitpack.io/")
+    }
+}
+rootProject.name = "RoyalFlush"
+include(":app")
+include(":feature:feature-map")
+include(":feature:feautre-subway")
+include(":feature:feature-teatime")
+include(":feature:feature-more")
+include(":feature:feature-detail")
+include(":feature:feature-login")
+include(":data:data-common")
+include(":data:data-toilet")
+include(":data:data-subway")
+include(":domain")
