5d73df8e5a26d286fde2eb7250e4c38c0137fd46
hoon <ldh019@naver.com>
2023-05-01 15:21:00 +0900

    [feature/navigation] #5 feat: Application & MainActivity 클래스 추가
    Co-authored-by: yangsooplus <yangsooplus@naver.com>
    Co-authored-by: bngsh <hee132@korea.ac.kr>
    Co-authored-by: swjw13 <swjw13@gmail.com>
    Co-authored-by: jeeminimini <jiminn821@naver.com>
    Co-authored-by: soopeach <hsjeon584@gmail.com>

--- app/src/main/java/com/ggne/royalflush/MainActivity.kt
+++ app/src/main/java/com/ggne/royalflush/MainActivity.kt
@@ -2,10 +2,32 @@ package com.ggne.royalflush
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.ggne.royalflush.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+    }
+    private val navController by lazy {
+        navHostFragment.navController
+    }
+    private val appBarConfiguration by lazy {
+        AppBarConfiguration(navController.graph)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.bnvMain.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
