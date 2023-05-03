d58919370dec6ff052e5f043c9e4f8c514a405ea
Hoon <ldh019@naver.com>
2023-05-03 15:52:29 +0900

    [feature/navigation] #5 chore: 중복된 라이브러리 추가 제거

--- feature/feature-subway/build.gradle.kts
+++ feature/feature-subway/build.gradle.kts
@@ -8,8 +8,6 @@ android {
     namespace = "com.ggne.feature_subway"
 }
 dependencies {
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     testImplementation(libs.bundles.test)
     androidTestImplementation(libs.bundles.test.android)
 }
