84c314b03e3b8c36920b48e535f25b807339b59a
soopeach <hsjeon584@gmail.com>
2023-05-10 12:47:53 +0900

    [feature/common/royal_flush_edit_text]: #9 feat: Royal Flush Edit Text Start Icon 속성 구현

--- feature/feature-common/src/main/java/com/ggne/feature_common/custom/RoyalFlushEditText.kt
+++ feature/feature-common/src/main/java/com/ggne/feature_common/custom/RoyalFlushEditText.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.ggne.feature_common.R
 import com.ggne.feature_common.databinding.RoyalFlushEditTextBinding
 
 class RoyalFlushEditText(context: Context, attrs: AttributeSet): ConstraintLayout(context, attrs) {
@@ -11,7 +12,30 @@ class RoyalFlushEditText(context: Context, attrs: AttributeSet): ConstraintLayou
     private val binding = RoyalFlushEditTextBinding.inflate(LayoutInflater.from(context), this, true)
 
     init {
+
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.RoyalFlushEditText,
+            0, 0
+        ).apply {
+            try {
+                getResourceId(R.styleable.RoyalFlushEditText_StartIcon, EMPTY_RESOURCE).let { resource ->
+                    if (resource == EMPTY_RESOURCE) {
+                        binding.startIc.visibility = GONE
+                    } else {
+                        binding.startIc.setImageResource(resource)
+                    }
+                }
+            } finally {
+                recycle()
+            }
+        }
+
         removeAllViews()
         addView(binding.root)
+    }
+
+    companion object {
+        private const val EMPTY_RESOURCE = -1
     }
 }
